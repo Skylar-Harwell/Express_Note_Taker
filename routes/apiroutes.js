b802b75173f7236c4c070744467b36dafe1846fe
Skylar Harwell <swharwell@gmail.com>
2021-09-02 18:15:40 -0400

    Updated folders and files for GET & POST functions

---
+++ routes/apiroutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const notes = require('../db/db.json');
+const fs = require('fs');
+const uuid = require('../helpers/uuid');
+
+// GET Route for retrieving all the feedback
+router.get('/notes', (req, res) => {
+  res.json(notes);
+});
+
+// POST Route for submitting feedback
+router.post('/notes', (req, res) => {
+    const id = uuid();
+    const newID = {id}
+    
+    Object.assign(req.body, newID);
+    console.log(req.body);
+
+    notes.push(req.body);
+
+    fs.writeFile(`./db/db.json`, JSON.stringify(notes), (err) =>
+      err
+        ? res.send(err)
+        : res.json(notes)
+    );  
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const api = require('./routes/apiroutes.js');
+
+const PORT = process.env.port || 3001;
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
